36e9a8b3a342cb85a8481792b3895c27d895a753
Hoa Dinh <hoadm.net@gmail.com>
2024-12-24 10:43:56 +0700

    update text-black in register form

--- resources/views/livewire/register.blade.php
+++ resources/views/livewire/register.blade.php
@@ -112,7 +112,7 @@
         </div>
 
         <div class="mb-4">
-            <label class="inline-flex items-center">
+            <label class="inline-flex items-center text-black">
                 <input
                     type="checkbox"
                     class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-offset-0 focus:ring-indigo-200 focus:ring-opacity-50"
@@ -123,7 +123,7 @@
         </div>
 
         <div class="mb-4">
-            <label class="inline-flex items-center">
+            <label class="inline-flex items-center text-black">
                 <input
                     type="checkbox"
                     class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-offset-0 focus:ring-indigo-200 focus:ring-opacity-50"
